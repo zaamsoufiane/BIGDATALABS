49f43239e565128596d43c069c363ab77446a251
zaamsoufiane <soufiane.zaam12@gmail.com>
2025-12-05 02:52:39 +0100

    Ajout lab4

---
+++ lab4_kafka/src/main/java/edu/supmti/kafka/EventConsumer.java
@@ -0,0 +1,37 @@
+package edu.supmti.kafka;
+
+import java.util.Properties;
+import java.util.Arrays;
+import java.time.Duration;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class EventConsumer {
+  public static void main(String[] args) throws Exception {
+    if (args.length == 0) {
+      System.out.println("Entrer le nom du topic");
+      return;
+    }
+    String topicName = args[0];
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", "test");
+    props.put("enable.auto.commit", "true");
+    props.put("auto.commit.interval.ms", "1000");
+    props.put("session.timeout.ms", "30000");
+    props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+    consumer.subscribe(Arrays.asList(topicName));
+    System.out.println("Souscris au topic " + topicName);
+
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+      for (ConsumerRecord<String, String> record : records) {
+        System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+      }
+    }
+  }
+}
